e4e9452b77f3f11b965d62af274009e8cd0e6bbc
pablo <mp.mezapablo@gmail.com>
2022-07-08 03:55:26 -0300

    ejercicio 2

---
+++ Ejercicio2.js
@@ -0,0 +1,15 @@
+listaEjercicio = [
+    'pablo',
+    30,
+    true,
+    10,
+    fecha_nacimiento = new Date('march 01 1991'),
+    libro={
+        titulo  :'Úselo y tírelo', 
+        autor   :'Eduardo Galeano', 
+        fecha   :1994, 
+        url     : new URL('https://shopee.com.ar/%C3%9Aselo-Y-T%C3%ADrelo-Eduardo-Galeano-i.770053715.21205604621?gclid=CjwKCAjwiJqWBhBdEiwAtESPaGYtqfElbN7yExVcNoqaa7U6HNzZjTOvL7-zDs6bX0Kju-gx9K5_VhoCa0QQAvD_BwE')
+    }
+]
+
+console.log(listaEjercicio)
