4cb8b624f8ed3f9cf359082c30031669589d41dd
pablo <mp.mezapablo@gmail.com>
2022-07-10 06:19:16 -0300

    actualizando modulo estuctura de control

---
+++ 03-Estructuras de control/If-else.js
@@ -0,0 +1,13 @@
+// Bifurcaciones if...else
+let saldo = 50;
+let pedidoExtraccion = 100;
+
+if (pedidoExtraccion < saldo) {
+    console.log("Puedes sacar dinero")
+}
+
+if (pedidoExtraccion < saldo) {
+    console.log("Puedes sacar dinero")
+} else {
+    console.log("Saldo insuficiente")
+}
