3de90c7faf6c97d2fcc2de4f551c37435766e076
pablo <mp.mezapablo@gmail.com>
2022-07-16 04:53:28 -0300

    actualizando buscar valores

--- 06-listas/buscar-valores-find.js
+++ 06-listas/buscar-valores-find.js
@@ -18,13 +18,14 @@ arrayNums.forEach( elemento => {
 })
 console.log(suma)           // 205  
 
+
+/*********  Encuentra un elemento *********/
 // find():
 // busca un elemento dentro de un array y retorna el primero que cumpla
 // con la condición especificada en la función callback.
 // Quiero encontrar el elemento 90
 // formas de uso:
 
-/*********  Encuentra un elemento *********/
 //menos eficiente:
 const variable = array.find(valor => {
     if (valor === 'hola') {
@@ -39,11 +40,11 @@ console.log(result);
 
 //Mas eficiente
 const listaPersonas = [
-    { nombre: "juan",   edad: 35 },
-    { nombre: "Raul",   edad: 34 },
-    { nombre: "Miguel", edad: 28 },
-    { nombre: "Lucía",  edad: 3 },
-    { nombre: "Analia", edad: 29}
+    { nombre: "Juan",   edad: 35, altura: 1.80 },
+    { nombre: "Raul",   edad: 34, altura: 1.82 },
+    { nombre: "Miguel", edad: 28, altura: 1.84 },
+    { nombre: "Lucía",  edad: 3 , altura: 1.85 },
+    { nombre: "Analia", edad: 29, altura: 1.86 }
 ]
 
 const persona1 = listaPersonas.find(perso => {
@@ -55,5 +56,7 @@ const persona2 = listaPersonas.find(perso => perso.nombre === "Raul")
 console.log(persona1);
 console.log(persona2);
 
-const { edad } = listaPersonas.find(perso => perso.nombre === "Miguel")
-console.log(edad)
+const { edad }      = listaPersonas.find(perso => perso.nombre === "Miguel")
+const { altura }    = listaPersonas.find(perso => perso.nombre === "Juan")
+console.log(edad)   // 28
+console.log(altura) // 1.8
